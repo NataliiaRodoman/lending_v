c0f9fdd3488f30515b564febe5dff8cb97954c01
Natali <swetswet1234@gmail.com>
2023-07-12 14:36:13 +0300

    end

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,22 @@
 $('.autoplay').slick({
-  slidesToShow: 3,
+  slidesToShow: 5,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 2000,
+  autoplaySpeed: 3000,
+  arrow: false,
+});
+
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 3
+});
+
+$('.slider').slick({
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: "linear",
 });
